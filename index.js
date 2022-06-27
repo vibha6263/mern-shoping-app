93e70c7be77aad9296008eaa1b22f8fde087027f
vibha mishra <TestEmail@mail.com>
2022-06-27 15:21:24 +0530

    vibha

--- index.js
+++ index.js
@@ -1,17 +1,7 @@
+require('./config/config')
+require('./models/db') 
 const express = require('express')
-const product = require('./assets/products.json')
 const app = express()
+app.use(express.json())
 
-app.get('/', function (req, res) {
-  res.send('hello')
-})
-app.get('/getProduct/', (req, res)=> {
-  let cat=[];
-  let vibha=product.filter((c)=>{
-    if(c.category==="jewelery")
-      cat.push(c);
-  })
-  res.send(cat);
-})
-
-app.listen(3000)
+app.listen(process.env.PORT)
