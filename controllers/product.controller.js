a81c31ffef2c3f721c3e0c64a4185cbf570346c4
vibha mishra <TestEmail@mail.com>
2022-07-01 15:32:29 +0530

    vibha

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -1,4 +1,23 @@
+  const mongoose = require("mongoose");
+  const productSchema = mongoose.model('productSchema')
+
 const products = require("../assets/products.json");
-module.exports.getProduct = (req,res)=>{
-    res.send(products)
+module.exports.addProduct = (req,res)=>{
+    
+    var data = new productSchema();
+    data.title=req.body.title
+    data.image =req.body.image
+    data.price = req.body.price
+    data.email = req.body.email
+    
+    data.save((err, doc) => {
+        if (!err)
+            res.send(doc);
+        else {
+                res.send(['Unable To Add Product Details.']);
+        }
+    
+    });
+    
+
 }
